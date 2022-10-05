60ca9ef1c36ba416b56b105bd7993cc307b1e435
Jorge Felix <jorge30fm@gmail.com>
2022-10-05 19:28:46 -0400

    .

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem.js';
+import ProductItem from '../ProductItem/index.js';
 import { useStoreContext } from '../../utils/GlobalState.js';
 import { UPDATE_PRODUCTS } from '../../utils/actions.js';
 import { useQuery } from '@apollo/client';
