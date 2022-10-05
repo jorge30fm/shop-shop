153f491ff79bfef5a504e438e8a443c6237e6756
Jorge Felix <jorge30fm@gmail.com>
2022-10-05 15:46:08 -0400

    handle payments and refractor code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,14 +2,15 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
+
 const Home = () => {
-	return (
-		<div className="container">
-			<CategoryMenu />
-			<ProductList />
+  return (
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
       <Cart />
-		</div>
-	);
+    </div>
+  );
 };
 
 export default Home;
