321a272f7794f122186267f025109a22b55461ec
Jorge Felix <jorge30fm@gmail.com>
2022-10-05 20:51:53 -0400

    change connection file"

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+
 });
 
 export default mongoose.connection;
